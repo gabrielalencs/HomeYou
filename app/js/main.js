d528fd26d65cba3d4fca5f7e7dd2e0742a13a51a
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-01-08 23:04:22 -0300

    develop mobile menu

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+// menu mobile header
+const containerMenuMobile = document.querySelector(".header__menu-mobile");
+const buttonMenuMobile = document.querySelector(".header__btn-menu");
+
+buttonMenuMobile.addEventListener("click", toggleMenuMobile);
+
+function toggleMenuMobile() {
+    containerMenuMobile.classList.toggle("active");
+    containerMenuMobile.style.top = containerMenuMobile.classList.contains("active") ? "6.5rem" : "3rem";
+}
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 922) {
+        containerMenuMobile.classList.remove("active");
+        containerMenuMobile.style.top = "3rem";
+    }
+});
