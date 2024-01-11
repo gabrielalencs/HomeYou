af663d68180f34116cf68b5bec1c4cd1d382d2df
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-01-11 14:25:09 -0300

    header javascript code refactor

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,3 @@
-// menu mobile header
 const containerMenuMobile = document.querySelector(".header__menu-mobile");
 const buttonMenuMobile = document.querySelector(".header__btn-menu");
 
@@ -6,14 +5,36 @@ buttonMenuMobile.addEventListener("click", toggleMenuMobile);
 
 function toggleMenuMobile() {
     containerMenuMobile.classList.toggle("active");
-    containerMenuMobile.style.top = containerMenuMobile.classList.contains("active") ? "6.5rem" : "3rem";
+    updateContainerTopStyle();
 
-    
+    document.addEventListener("click", handleDocumentClick);
+    window.addEventListener("resize", handleWindowResize);
 }
 
-window.addEventListener("resize", () => {
-    if (window.innerWidth > 922) {
-        containerMenuMobile.classList.remove("active");
-        containerMenuMobile.style.top = "3rem";
+function updateContainerTopStyle() {
+    const topStyle = containerMenuMobile.classList.contains("active") ? "6.5rem" : "3rem";
+    containerMenuMobile.style.top = topStyle;
+}
+
+// handle clicks outside the menu to close the menu
+function handleDocumentClick(event) {
+    const isClickInsideMenu = containerMenuMobile.contains(event.target);
+    const isClickOnMenuButton = buttonMenuMobile.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnMenuButton) {
+        toggleMenuMobile();
+        
+        document.removeEventListener("click", handleDocumentClick);
     }
-});
+}
+
+// handle window resizing to close mobile menu
+function handleWindowResize() {
+    if (window.innerWidth > 922) {
+        toggleMenuMobile();
+        
+        window.removeEventListener("resize", handleWindowResize);
+    } else {
+        updateContainerTopStyle();
+    }
+}
